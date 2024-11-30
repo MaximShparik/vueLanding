e2cc48905ae568df6fa463522b72bc043d3f4b24
maxim shparik <maxim.shparik@gmail.com>
2024-11-30 15:13:48 +0300

    update

--- src/components/NavbarElement.vue
+++ src/components/NavbarElement.vue
@@ -1,41 +1,28 @@
 <template>
     <!-- Navbar start -->
-    <nav class="w-screen fixed bg-opacity-90 bg-white p-4 lg:px-20 flex justify-between z-50">
-		<!-- Logo start -->
-		<div class="h-full flex flex-col justify-center">
-			<p class="tracking-widest font-semibold">LANDING</p>
-			<p class="tracking-widest font-light">PHOTOGRAPHY</p>
-		</div>
-		<!-- Logo end -->
-
-		<!-- Toggler start -->
-		<button role="menubar" class="md:hidden" @click="navToggle()">
-			<i class="fa-solid fa-bars text-2xl"></i>
-		</button>
-		<!-- Toggler end -->
-
-		<!-- Menu content start -->
-		<div class="absolute left-0 right-0 translate-y-16 bg-opacity-90 bg-white md:bg-none md:bg-opacity-0 shadow hidden md:flex flex-col gap-4 items-center p-4 md:flex-row md:static md:shadow-none md:translate-y-0" role="menu" aria-expanded="false">
-			<!-- Links start -->
-			<a href="#" role="menuitem">
-				<p class="mx-4">Home</p>
-			</a>
-
-			<a href="#" role="menuitem">
-				<p class="mx-4">About</p>
-			</a>
-
-			<a href="#" role="menuitem">
-				<p class="mx-4">Explore</p>
-			</a>
-
-			<button class="btn" role="menuitem">
-				Contact us
-			</button>
-			<!-- Links end -->
-		</div>
-		<!-- Menu content end -->
-	</nav>
+		<nav class="w-screen fixed p-4 lg:px-20 flex z-50">
+			<div class="navItem">
+				<img src="@/assets/tg.png" alt="">
+			</div>
+			<div class="navItem">
+				<img src="@/assets/tg.png" alt="">
+			</div>
+			<div class="navItem">
+				<img src="@/assets/tg.png" alt="">
+			</div>
+			<div class="navItem">
+				<img src="@/assets/tg.png" alt="">
+			</div>
+			<div class="contractInfo">
+				<div class="contract">
+					H4PDo8ngWwC4quPTRWfTr2HorUQ2Ep4G3JVeJHMfkZAT
+				</div>
+				<div class="info">
+					<div class="text">Contract address</div>
+					<div class="copy"><img src="@/assets/copy.png" alt=""></div>
+				</div>
+			</div>
+		</nav>
     <!-- Navbar end -->
 </template>
 
@@ -55,6 +42,44 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+nav {
+	justify-content: flex-end;
+}
+.navItem {
+	width: 40px;
+	height: 40px;
+	background: #00fc00;
+	border-radius: 50%;
+	margin-right: 30px;
+	margin-top: 25px;
+	cursor: pointer;
+}
+.info {
+	display: flex;
+	flex-direction: row;
+	background: green;
+	padding: 5px;
+	border-radius: 0px 0px 17px 17px;
+	justify-content: space-around;
+}
+.copy {
+	cursor: pointer;
+}
+.copy img {
+	width: 20px;
+}
+.contractInfo {
+	border: 2px solid red;
+	border-radius: 20px;
+}
+.contract {
+	background: grey;
+	padding: 10px;
+	border-radius: 17px 17px 0px 0px;
+	color: white;
+}
+.text {
+	color: black;
+}
 </style>
--- src/sections/MainSection.vue
+++ src/sections/MainSection.vue
@@ -1,14 +1,61 @@
 <template>
 
 	<section class="h-screen px-12 lg:px-32 py-32">
-		<div class="w-full md:w-1/2 h-full flex flex-col md:justify-center ">
-			<h1 class="uppercase text-7xl font-semibold" style="color: #f90000;">
-				SOSAL GUBAMI ???
-			</h1>
+		<div class="w-full md:w-1/3 h-full flex flex-col md:justify-center info">
+			<p class="gubamiText">
+				$GUBAMI
+			</p>
+			<p class="sosalText">
+				SOSAL?)
+			</p>
+			<div class="mainSOSAL">
+				<img src="@/assets/mainSOSAL.jpeg" alt="gubami">
+			</div>
+			<div class="menu">
+				<div class="menuItem" @click="showInfo('vote')">Vote</div>
+				<div class="menuItem" @click="showInfo('meme')">Meme meaning</div>
+				<div class="menuItem" @click="showInfo('token')">Tokenomics</div>
+				<!-- <div class="menuItem" @click="showInfo('discord')">Discord</div> -->
+			</div>
+		</div>
+
+		<div class="content">
+			<div class="contentItem vote">
+				<div class="voteText">
+					We wanna spread the word, so I created Urban dictionary
+					definition, please vote. Simply press like on the site
+				</div>
+				<div class="voteBtns">
+					<div class="hands">
+						<div><img src="@/assets/hands2.png" alt=""></div>
+						<div><img src="@/assets/hands1.png" alt=""></div>
+					</div>
+					<div class="logo">
+						<img src="@/assets/urban.png" alt="">
+					</div>
+				</div>
+			</div>
+			<div class="contentItem meme">
+				The original name "GUBAMI" refers to an emote on Twitch, but I felt that this meme
+				deserved more than just being a local emote on some streamers' channels. The origin story
+				is quite amusing: there's a long-standing joke where you bait someone into answering
+				"yes" to a question and, right before they respond, you quickly ask, "SOSAL?" ("Did you
+				suck?" - referring to oral activity), making their answer "yes" apply to that question. If
+				they "SOSAL" (sucked) then they definitely did it with "GUBAMI" (which loosely
+				translates to "sucked with lips"). Mog type shit. After countless jokes of this kind on
+				Twitch, this emote emerged. Below, I'll provide examples of how this played out.
+				In recent months, this joke has frequently appeared in the local "Deadlock" game
+				community. So original Twith emote is about 3 month old. Deadlock is amazing btw. So, to
+				answer the question of who is depicted in the emote: it's Abrams with huge lips, a character
+				from the game.
+			</div>
+			<div class="contentItem token">
+				Tokenomics TokenomicsTokenomicsTokenomicsTokenomics Tokenomics Tokenomics TokenomicsTokenomicsTokenomicsTokenomicsTokenomics Tokenomics TokenomicsTokenomics Tokenomics Tokenomics Tokenomics Tokenomics Tokenomics м Tokenomics Tokenomicsм Tokenomicsvм мм Tokenomicsv Tokenomics
+			</div>
 		</div>
 
 		<video
-			src="@/assets/bg.mp4"
+			src="@/assets/bg.webm"
 			class="video"
 			playsinline
 			loop
@@ -16,46 +63,65 @@
 			autoplay
 		></video>
 
-		<img id="flying-image" src="@/assets/girl.jpeg" alt="Flying Image" />
+		<!-- <img id="flying-image" src="@/assets/girl.jpeg" alt="Flying Image" /> -->
 
 	</section>
 </template>
 
 <script>
 export default {
-	// methods: {
-	// 	flySosal () {
+	methods: {
+		showInfo (tag) {
+			if (tag === 'vote') {
+				document.querySelector('.vote').style.display = 'block'
+				document.querySelector('.meme').style.display = 'none'
+				document.querySelector('.token').style.display = 'none'
+			}
+			if (tag === 'meme') {
+				document.querySelector('.vote').style.display = 'none'
+				document.querySelector('.meme').style.display = 'block'
+				document.querySelector('.token').style.display = 'none'
+			}
+			if (tag === 'token') {
+				document.querySelector('.vote').style.display = 'none'
+				document.querySelector('.meme').style.display = 'none'
+				document.querySelector('.token').style.display = 'block'
+			}
+		}
+	},
+	// mounted () {
 
-	// 	}
-	// },
-	mounted () {
+	// 	const flyingImage = document.getElementById('flying-image');
 
-		const flyingImage = document.getElementById('flying-image');
-
-		function getRandomPosition() {
-			const imageWidth = flyingImage.offsetWidth;
-			const imageHeight = flyingImage.offsetHeight;
+	// 	function getRandomPosition() {
+	// 		const imageWidth = flyingImage.offsetWidth;
+	// 		const imageHeight = flyingImage.offsetHeight;
 			
-			// Вычисляем доступные координаты с учётом размеров изображения
-			const x = Math.random() * (window.innerWidth - imageWidth);
-			const y = Math.random() * (window.innerHeight - imageHeight);
-			return { x, y };
-		}
+	// 		// Вычисляем доступные координаты с учётом размеров изображения
+	// 		const x = Math.random() * (window.innerWidth - imageWidth);
+	// 		const y = Math.random() * (window.innerHeight - imageHeight);
+	// 		return { x, y };
+	// 	}
 
-		function moveImage() {
-			const { x, y } = getRandomPosition();
-			flyingImage.style.transform = `translate(${x}px, ${y}px)`;
-		}
+	// 	function moveImage() {
+	// 		const { x, y } = getRandomPosition();
+	// 		flyingImage.style.transform = `translate(${x}px, ${y}px)`;
+	// 	}
 
-		moveImage()
+	// 	moveImage()
 
-		// Запускаем анимацию каждые 2 секунды
-		setInterval(moveImage, 2000);
-	}
+	// 	// Запускаем анимацию каждые 2 секунды
+	// 	setInterval(moveImage, 2000);
+	// }
 }
 </script>
 
 <style>
+section {
+	display: flex;
+	flex-direction: row;
+	justify-content: space-between;
+}
 .video {
   position: absolute;
 	top: 0;
@@ -72,7 +138,66 @@ export default {
 	top: 0;
   width: 100px; /* Размер изображения */
   height: auto;
-  transition: transform 1s ease-in-out;
+  transition: transform 5s ease-in-out;
 }
-
+.gubamiText {
+	font-size: 60px;
+	font-weight: 700;
+	color: red;
+}
+.sosalText {
+	font-size: 40px;
+	font-weight: 700;
+	color: red;
+}
+.info {
+	text-align: center;
+}
+.mainSOSAL {
+	width: 200px;
+	margin: 20px auto;
+}
+.mainSOSAL img {
+	border-radius: 50%;
+}
+.menuItem {
+	color: white;
+	font-size: 40px;
+	font-weight: 400;
+	cursor: pointer;
+}
+.content {
+	display: flex;
+	flex-direction: column;
+	justify-content: flex-end;
+	width: 600px;
+}
+.contentItem {
+	background: #000000ab;
+	padding: 20px;
+	color: white;
+}
+.contentItem > * {
+	color: white;
+}
+.meme {
+	display: none;
+}
+.token {
+	display: none;
+}
+.voteBtns {
+	display: flex;
+	justify-content: space-around;
+	margin-top: 30px;
+}
+.hands {
+	display: flex;
+	width: 150px;
+	justify-content: space-around;
+	border: 2px solid white;
+	border-radius: 30px;
+	padding: 10px;
+	cursor: pointer;
+}
 </style>
